feec75755d782e45a8fefa1e093f802a364704be
pemulis <jds@riseup.net>
2017-10-19 11:07:21 -0700

    root to books index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   scope '/api' do
     resources :books
   end
+
+  root to: 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
