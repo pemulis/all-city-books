68d989900b9673d486b93eec607d305747531593
pemulis <jds@riseup.net>
2017-10-19 18:31:45 -0700

    make a React component called BookList and load the book list from the database to display, making it possible to select specific books from the list to see more detailed information about it

---
+++ app/javascript/components/BookList.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+export default class BookList extends React.Component {
+  /*
+  componentDidMount() {
+    window.fetch('api/books')
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.log(error))
+  }
+  render() {
+    return (
+        <div className="App">
+          <div className="App-header">
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/BookList.js</code> and save to reload.
+          </p>
+        </div>
+      );
+    }
+  }
+  */
+  constructor() {
+    super()
+    this.state = {}
+    this.getBooks = this.getBooks.bind(this)
+    this.getBook = this.getBook.bind(this)
+  }
+  componentDidMount() {
+    this.getBooks()
+  }
+  fetch (endpoint) {
+    return new Promise((resolve, reject) => {
+      window.fetch(endpoint)
+      .then(response => response.json())
+      .then(json => resolve(json))
+      .catch(error => reject(error))
+    });
+  }
+  getBooks () {
+    this.fetch('api/books')
+      .then(books => {
+        this.setState({books: books})
+        this.getBook(books[0].id)
+      });
+  }
+  getBook (id) {
+    this.fetch(`api/books/${id}`)
+      .then(book => this.setState({book: book}))
+  }
+  render() {
+    let {books, book} = this.state
+    return books
+
+    ? <div>
+        <h1>All City Books</h1>
+        {book &&
+        <div>
+          <h2>{book.itemname}</h2>
+          {book.price = book.price.toLocaleString('en-US', { style: 'currency'}) && <p>Price: ${book.price}</p>}
+          {book.binding && <p>Binding: {book.binding}</p>}
+          {<p>Quantity: {book.quantity}</p>}
+          {book.itemnote && <p>Item Note: {book.itemnote}</p>}
+          {book.asin1 && <p>ASIN/ISBN: {book.asin1}</p>}
+        </div>
+        }
+        <ul>
+          {Object.keys(books).map((key) => {
+            return <li active={book && book.id === books[key].id} key={key} onClick={() => this.getBook(books[key].id) }>
+              <a>{books[key].itemname}</a>
+            </li>
+          })}
+        </ul>
+      </div>
+    : <div> 
+        <p>Loading...</p>
+      </div>
+  }
+}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,12 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+console.log('Hello World from Webpacker');
+import React from 'react';
+import { render } from 'react-dom';
+import BookList from '../components/BookList';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.body.appendChild(document.createElement('div'));
+  render(<BookList/>, container);
+});
